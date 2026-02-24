b000fff575e26c7e9cc4b10220509dac9f9f8b0a
surajdito <anoopsurajkbtech24@rvu.edu.in>
2026-02-24 17:05:27 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -45,7 +45,7 @@ model = SimpleNN(input_size=20)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-EPOCHS = 200
+EPOCHS = 250
 
 print("Training model...\n")
 
